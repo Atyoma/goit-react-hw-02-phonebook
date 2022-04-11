ec1c879f03745a0b49161bc97dc99d131ceefe08
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-04-11 10:29:33 +0300

    scale

--- src/components/ContactList.styled.js
+++ src/components/ContactList.styled.js
@@ -39,6 +39,6 @@ export const DelBtn = styled.button`
     -23px 0 20px -23px rgba(0, 0, 0, 0.6), 23px 0 20px -23px rgba(0, 0, 0, 0.6),
     inset 0 0 40px rgba(0, 0, 0, 0.1);
   :hover {
-    transform: scale(1.1);
+    transform: scale(1.2);
   }
 `;
